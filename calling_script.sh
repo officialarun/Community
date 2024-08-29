0fdfe695cdf74e0d0290c4338b66771f9bb8a46e
officialarun <officialarunpandey0600@gmail.com>
2024-08-30 00:07:29 +0530

    Adding changes...

--- calling_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-current_user=$(whoami)
-
-#calling script to log latest entry:
-/home/arunlinux/Desktop/shell/record.sh "${0}" "$current_user"
-
-echo "This script is logged successfully into the logger.txt file "
-
